a8db4e7ea71db56ed98e10ce3bb30a65d7c5554c
Jacob Jahneke <jacobjahneke@gmail.com>
2018-06-13 07:34:39 -0500

    working with no global var

--- demo.js
+++ demo.js
@@ -1,15 +1,15 @@
-
-let playForLoss = Math.floor(Math.random() * (0 - -4) + -4);
+/*let playForLoss = Math.floor(Math.random() * (0 - -4) + -4);
 let shortRunGain = Math.floor(Math.random() * (4 - 1) + 1); 
 let medRunGain = Math.floor(Math.random() * (7 - 5) + 5);
 let longRunGain = Math.floor(Math.random() * (15 - 7) + 7);
 let shortPassGain = Math.floor(Math.random() * (6 - 4) + 4);
 let medPassGain = Math.floor(Math.random() * (12 - 7) + 7);
 let longPassGain = Math.floor(Math.random() * (20 - 13) + 13);
-
+*/
 
 function gameIntro() {
 	//result variable
+	
 	let diceRandomNumber;
 	//intro
 	alert("Welcome to the Red Zone Challenge... [Instructions: You have four downs to score a TD in the Redzone... Pick your play and best of luck!]");
@@ -19,7 +19,7 @@ function gameIntro() {
 	firstDown (playerName, diceRandomNumber);
 	}
 
-function firstDown (playerName, diceRandomNumber, randomSixSided, failSixSided, playResult) {
+function firstDown (playerName, diceRandomNumber, playResult) {
 	let playSelection;
 	playSelection = prompt(""+ playerName +"... FIRST DOWN!!!, Which play would you like to run...  {Short Run: Enter 1, Medium Run: Enter 2, Long Run: Enter 3, Short Pass: Enter 4, Medium Pass: Enter 5, Long Pass: Enter 6}");
 	let resultOne;
@@ -29,63 +29,27 @@ function firstDown (playerName, diceRandomNumber, randomSixSided, failSixSided, 
 	let resultFive;
 	let resultSix;
 	switch (true) {
-			case playSelection === "1":
-			resultOne = rollDie(6);
-			if (resultOne > 1) {
-				playResult = shortRunGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+		case playSelection === "1":
+			playResult= checkResultOne();
 			break;
 		case playSelection === "2":
-		    resultTwo = rollDie(8);
-		    if (resultTwo > 3) {
-				playResult = medRunGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+		    playResult= checkResultTwo();
 		    break;
 		case playSelection === "3":
-			resultThree = rollDie(12);
-			if (resultThree > 6) {
-				playResult = longRunGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+			playResult= checkResultThree();
 			break;
 		case playSelection === "4":
-		    resultFour = rollDie(8);
-		    if (resultFour > 2) {
-				playResult = shortPassGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+		    playResult= checkResultFour();
 		    break;
 		case playSelection === "5":
-			resultFive = rollDie(12);
-			if (resultFive > 5) {
-				playResult = medPassGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+			playResult= checkResultFive();
 			break;
 		case playSelection === "6":
-		    resultSix = rollDie(20);
-		    if (resultSix > 10) {
-				playResult = longPassGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+		    playResult= checkResultSix();
 		    break;
 		default: 
-			alert("What are you doing guy!");
-			break;			
+		alert("What are you doing guy!");
+			break;						
 	}
 
 	
@@ -98,7 +62,7 @@ function firstDown (playerName, diceRandomNumber, randomSixSided, failSixSided, 
 	secondDown (playerName, diceRandomNumber, yardsToTouchdown);
 }
 
-function secondDown (playerName, diceRandomNumber, yardsToTouchdown) {
+function secondDown (playerName, diceRandomNumber, yardsToTouchdown, playResult) {
 	let playSelection;
 	playSelection = prompt(""+ playerName +"...... SECOND DOWN!!!, Which play would you like to run...  {Short Run: Enter 1, Medium Run: Enter 2, Long Run: Enter 3, Short Pass: Enter 4, Medium Pass: Enter 5, Long Pass: Enter 6}");
 	let resultOne;
@@ -109,62 +73,26 @@ function secondDown (playerName, diceRandomNumber, yardsToTouchdown) {
 	let resultSix;
 	switch (true) {
 		case playSelection === "1":
-			resultOne = rollDie(6);
-			if (resultOne > 1) {
-				playResult = shortRunGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+			playResult= checkResultOne();
 			break;
 		case playSelection === "2":
-		    resultTwo = rollDie(8);
-		    if (resultTwo > 3) {
-				playResult = medRunGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+		    playResult= checkResultTwo();
 		    break;
 		case playSelection === "3":
-			resultThree = rollDie(12);
-			if (resultThree > 6) {
-				playResult = longRunGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+			playResult= checkResultThree();
 			break;
 		case playSelection === "4":
-		    resultFour = rollDie(8);
-		    if (resultFour > 2) {
-				playResult = shortPassGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+		    playResult= checkResultFour();
 		    break;
 		case playSelection === "5":
-			resultFive = rollDie(12);
-			if (resultFive > 5) {
-				playResult = medPassGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+			playResult= checkResultFive();
 			break;
 		case playSelection === "6":
-		    resultSix = rollDie(20);
-		    if (resultSix > 10) {
-				playResult = longPassGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+		    playResult= checkResultSix();
 		    break;
 		default: 
-			alert("What are you doing guy!");
-			break;		
+		alert("What are you doing guy!");
+			break;											
 	}
 
 	
@@ -179,7 +107,7 @@ function secondDown (playerName, diceRandomNumber, yardsToTouchdown) {
 	}
 }
 
-function thirdDown (playerName, diceRandomNumber, yardsToTouchdown, secondYardsToTouchdown) {
+function thirdDown (playerName, diceRandomNumber, yardsToTouchdown, secondYardsToTouchdown, playResult) {
 	let playSelection;
 	playSelection = prompt(""+ playerName +"THIRD DOWN!!!, Which play would you like to run...  {Short Run: Enter 1, Medium Run: Enter 2, Long Run: Enter 3, Short Pass: Enter 4, Medium Pass: Enter 5, Long Pass: Enter 6}");
 	let resultOne;
@@ -187,66 +115,31 @@ function thirdDown (playerName, diceRandomNumber, yardsToTouchdown, secondYardsT
 	let resultThree;
 	let resultFour;
 	let resultFive;
-	let resultSix;2
+	let resultSix;
 
 	switch (true) {
+	case playSelection === "1":
 		case playSelection === "1":
-			resultOne = rollDie(6);
-			if (resultOne > 1) {
-				playResult = shortRunGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+			playResult= checkResultOne();
 			break;
 		case playSelection === "2":
-		    resultTwo = rollDie(8);
-		    if (resultTwo > 3) {
-				playResult = medRunGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+		    playResult= checkResultTwo();
 		    break;
 		case playSelection === "3":
-			resultThree = rollDie(12);
-			if (resultThree > 6) {
-				playResult = longRunGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+			playResult= checkResultThree();
 			break;
 		case playSelection === "4":
-		    resultFour = rollDie(8);
-		    if (resultFour > 2) {
-				playResult = shortPassGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+		    playResult= checkResultFour();
 		    break;
 		case playSelection === "5":
-			resultFive = rollDie(12);
-			if (resultFive > 5) {
-				playResult = medPassGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+			playResult= checkResultFive();
 			break;
 		case playSelection === "6":
-		    resultSix = rollDie(20);
-		    if (resultSix > 10) {
-				playResult = longPassGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+		    playResult= checkResultSix();
 		    break;
 		default: 
-			alert("What are you doing guy!");
-			break;		
+		alert("What are you doing guy!");
+			break;			
 	}
 
 	
@@ -257,11 +150,12 @@ function thirdDown (playerName, diceRandomNumber, yardsToTouchdown, secondYardsT
 	else {
 	alert(playResult + "yards");
 	alert(""+ playerName +"... You have "+ thirdYardsToTouchdown +" yards to a TD!");
+
 	fourthDown (playerName, diceRandomNumber, yardsToTouchdown, secondYardsToTouchdown, thirdYardsToTouchdown);
 	}
 }
 
-function fourthDown (playerName, diceRandomNumber, yardsToTouchdown, secondYardsToTouchdown, thirdYardsToTouchdown) {
+function fourthDown (playerName, diceRandomNumber, yardsToTouchdown, secondYardsToTouchdown, thirdYardsToTouchdown, playResult) {
 	let playSelection;
 	playSelection = prompt(""+ playerName +"... FINAL PLAY... FOURTH DOWN!... Which play would you like to run (Short Run: Enter 1, Medium Run: Enter 2, Long Run: Enter 3, Short Pass: Enter 4, Medium Pass: Enter 5, Long Pass: Enter 6)");
 	let resultOne;
@@ -271,67 +165,31 @@ function fourthDown (playerName, diceRandomNumber, yardsToTouchdown, secondYards
 	let resultFive;
 	let resultSix;
 	switch (true) {
-		case playSelection === "1":
-			resultOne = rollDie(6);
-			if (resultOne > 1) {
-				playResult = shortRunGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+	case playSelection === "1":
+			case playSelection === "1":
+			playResult= checkResultOne();
 			break;
 		case playSelection === "2":
-		    resultTwo = rollDie(8);
-		    if (resultTwo > 3) {
-				playResult = medRunGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+		    playResult= checkResultTwo();
 		    break;
 		case playSelection === "3":
-			resultThree = rollDie(12);
-			if (resultThree > 6) {
-				playResult = longRunGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+			playResult= checkResultThree();
 			break;
 		case playSelection === "4":
-		    resultFour = rollDie(8);
-		    if (resultFour > 2) {
-				playResult = shortPassGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+		    playResult= checkResultFour();
 		    break;
 		case playSelection === "5":
-			resultFive = rollDie(12);
-			if (resultFive > 5) {
-				playResult = medPassGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+			playResult= checkResultFive();
 			break;
 		case playSelection === "6":
-		    resultSix = rollDie(20);
-		    if (resultSix > 10) {
-				playResult = longPassGain;
-			}
-			else {
-				playResult = playForLoss;
-			}
+		    playResult= checkResultSix();
 		    break;
 		default: 
-			alert("What are you doing guy!");
-			break;		
+		alert("What are you doing guy!");
+			break;			
 	}
 
-	
-	let fourthYardsToTouchdown = thirdYardsToTouchdown - playResult;
+		let fourthYardsToTouchdown = thirdYardsToTouchdown - playResult;
 	if (fourthYardsToTouchdown < 1) {
 	 	alert("Coming through in the Clutch! TOUCHDOWN!!!");
 	}
@@ -339,6 +197,105 @@ function fourthDown (playerName, diceRandomNumber, yardsToTouchdown, secondYards
 	alert(playResult + "yards");
 	alert(""+ playerName +"...FAIL!!! You fell "+ thirdYardsToTouchdown +" short yards of a TD!");
 	}
+
+	}
+
+function checkResultOne(resultOne, shortRunGain, playForLoss) {
+	if (rollDie(6) > 1) {
+				playResult = randomYardsPerPlayShortRun();
+			}
+			else {
+				playResult = randomYardsPerPlayLoss();
+			}
+	return playResult;
+}
+
+function checkResultTwo(resultTwo, medRunGain, playForLoss) {
+	if (rollDie(8) > 3) {
+				playResult = randomYardsPerPlayMedRun();
+			}
+			else {
+				playResult = randomYardsPerPlayLoss();
+			}
+	return playResult;
+}
+
+function checkResultThree(resultThree, longRunGain, playForLoss) {
+	if (rollDie(12) > 6) {
+				playResult = randomYardsPerPlayLongRun();
+			}
+			else {
+				playResult = randomYardsPerPlayLoss();
+			}
+	return playResult;
+}
+
+function checkResultFour(resultFour, shortPassGain, playForLoss) {
+	if (rollDie(8) > 2) {
+				playResult = randomYardsPerPlayShortPass();
+			}
+			else {
+				playResult =randomYardsPerPlayLoss();
+			}
+	return playResult;
+}
+
+function checkResultFive(resultFive, medPassGain, playForLoss) {
+	if (rollDie(12) > 5) {
+				playResult = randomYardsPerPlayMedPass();
+			}
+			else {
+				randomYardsPerPlayLoss();
+				playResult = randomYardsPerPlayLoss();
+			}
+	return playResult;
+}
+
+function checkResultSix(resultSix, longPassGain, playForLoss) {
+	if (rollDie(20) > 10) {
+				playResult = randomYardsPerPlayLongPass();
+			}
+			else {
+				playResult = randomYardsPerPlayLoss();
+			}
+	return playResult;
+}
+
+
+function randomYardsPerPlayLoss() {
+	let playForLoss = Math.floor(Math.random() * (0 - -4) + -4);
+	return playForLoss;
+}
+
+function randomYardsPerPlayShortRun() {
+	let shortRunGain = Math.floor(Math.random() * (4 - 1) + 1);
+	return shortRunGain
+}
+
+function randomYardsPerPlayMedRun() {
+	let medRunGain = Math.floor(Math.random() * (7 - 5) + 5);
+	return medRunGain;
+}
+
+function randomYardsPerPlayLongRun() {
+	let longRunGain = Math.floor(Math.random() * (15 - 7) + 7);
+	return longRunGain;
+}
+
+function randomYardsPerPlayShortPass () {
+	let shortPassGain = Math.floor(Math.random() * (6 - 4) + 4);
+	return shortPassGain;
+}
+
+function randomYardsPerPlayMedPass () {
+	let medPassGain = Math.floor(Math.random() * (6 - 4) + 4);
+	return medPassGain;
+}
+
+function randomYardsPerPlayLongPass () {
+	let longPassGain = Math.floor(Math.random() * (20 - 13) + 13);
+	return longPassGain;
+
 }
 
 function rollDie(playSelection) {
@@ -346,5 +303,4 @@ function rollDie(playSelection) {
 	diceRandomNumber = Math.ceil(Math.random() * (playSelection));
 	return diceRandomNumber;
 }
-	
-gameIntro ();
+gameIntro();
